6d6cd09f778c624eba1c884d388f38223932b24e
Yusuke Sakurai <y_sakurai@nexus-nt.co.jp>
2024-02-27 16:19:32 +0900

    検索するメソッドを追加

--- src/main/java/com/nexus/whc/repository/UserRepository.java
+++ src/main/java/com/nexus/whc/repository/UserRepository.java
@@ -17,7 +17,11 @@ public class UserRepository {
 	public List<Map<String, Object>> allUserInfo() {
 
 		// SQL文の作成
-		String sql = "SELECT * FROM m_user WHERE delete_flg = 0 ORDER BY user_id ASC";
+		String sql = "SELECT * FROM m_user \n"
+				+ "INNER JOIN m_authority\n"
+				+ "ON m_user.auth_id = m_authority.auth_id\n"
+				+ "WHERE delete_flg = 0 \n"
+				+ "ORDER BY user_id ASC";
 
 		// クエリを実行
 		List<Map<String, Object>> allUserList = jdbcTemplate.queryForList(sql);
@@ -25,4 +29,25 @@ public class UserRepository {
 		// 取得したリストを返す
 		return allUserList;
 	}
+
+	// ユーザマスタ＋権限マスタから検索条件によって情報を取得する
+	public List<Map<String, Object>> allUserSearch() {
+
+		// SQL文の作成
+		String sql = "SELECT * FROM m_user \n"
+				+ "INNER JOIN m_authority\n"
+				+ "ON m_user.auth_id = m_authority.auth_id\n"
+				+ "WHERE delete_flg = 0\n"
+				+ "And user_id = ? \n"
+				+ "OR user_name = ? \n"
+				+ "OR m_authority.auth_status = ? \n"
+				+ "OR mail_address = ? \n"
+				+ "ORDER BY user_id ASC";
+		// クエリを実行
+		List<Map<String, Object>> allUserSearchList = jdbcTemplate.queryForList(sql);
+
+		// 取得したリストを返す
+		return allUserSearchList;
+	}
+
 }
