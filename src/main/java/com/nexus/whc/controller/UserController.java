83382ec7c6463430861faa9b3d3bf026d549dc62
Yusuke Sakurai <y_sakurai@nexus-nt.co.jp>
2024-02-27 16:18:42 +0900

    画面側に値を送る処理を追加

--- src/main/java/com/nexus/whc/controller/UserController.java
+++ src/main/java/com/nexus/whc/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.nexus.whc.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nexus.whc.services.UserService;
 
 /*
  * UserController.java
@@ -18,9 +24,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	// データベース操作クラス
+	UserService userService;
+
 	@GetMapping("/list")
 	public String userList(Model model) {
 
+		// ユーザ情報を取得(削除されていない)
+		List<Map<String, Object>> allUserList = userService.allUserInfo();
+
+		// リクエストスコープに保存
+		model.addAttribute("allUserList", allUserList);
+		
+		// ユーザマスタ一覧に遷移
 		return "SMSUS001";
 	}
 }
