f2ba982519c362f8e4c5b562b73b5a0d071a13ee
Yusuke Sakurai <y_sakurai@nexus-nt.co.jp>
2024-02-28 13:18:15 +0900

    エラーメッセージの表示

--- src/main/java/com/nexus/whc/controller/UserController.java
+++ src/main/java/com/nexus/whc/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.nexus.whc.controller;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -29,6 +32,10 @@ public class UserController {
 	// データベース操作クラス
 	UserService userService;
 
+	@Autowired
+	// メッセージ格納
+	MessageSource messageSource;
+
 	// ユーザの一覧表示
 	@GetMapping("/list")
 	public String userList(Model model) {
@@ -44,13 +51,12 @@ public class UserController {
 	}
 
 	// ユーザ検索
-	@RequestMapping("/list")
+	/*	@RequestMapping("/list")*/
+	@PostMapping("/list")
 	public String allUserSearch(@RequestParam(name = "userId", defaultValue = "") String userId,
 			@RequestParam(name = "userName", defaultValue = "") String userName,
 			@RequestParam(name = "permission", defaultValue = "") String authStatus,
 			@RequestParam(name = "mailAddress", defaultValue = "") String mailAddress, Model model) {
-
-		System.out.println("検索メソッドの実行");
 
 		// リクエストスコープに検索条件を保存
 		model.addAttribute("userId", userId);
@@ -61,6 +67,14 @@ public class UserController {
 		// ユーザを検索
 		List<Map<String, Object>> allUserList = userService.allUserSearch(userId, userName, authStatus, mailAddress);
 
+		// 検索結果が0件の場合
+		if (allUserList.isEmpty()) {
+			String errorMessage = messageSource.getMessage("COM01W001", new String[] { "ユーザーマスタ" },
+					Locale.getDefault());
+			// リクエストスコープにエラーメッセージを保存
+			model.addAttribute("message", errorMessage);
+			return "SMSUS001";
+		}
 		// リクエストスコープに検索結果のリストを保存
 		model.addAttribute("allUserList", allUserList);
 
