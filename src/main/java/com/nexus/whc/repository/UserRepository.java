1bee01d8021eb55490629dc076f5e24f1a49949a
Yusuke Sakurai <y_sakurai@nexus-nt.co.jp>
2024-02-29 13:09:01 +0900

    検索方法の修正

--- src/main/java/com/nexus/whc/repository/UserRepository.java
+++ src/main/java/com/nexus/whc/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.nexus.whc.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -33,35 +34,48 @@ public class UserRepository {
 	// ユーザマスタ＋権限マスタから検索条件によって情報を取得する
 	public List<Map<String, Object>> allUserSearch(String userId, String userName,
 			String authStatus, String mailAddress) {
+		// TODO 改善点
 
 		// SQL文の作成
 		String sql = "SELECT * FROM m_user \n"
-				+ "INNER JOIN m_authority\n"
+				+ "INNER JOIN m_authority \n"
 				+ "ON m_user.auth_id = m_authority.auth_id\n"
-				+ "WHERE (user_id = ? \n"
-				+ "OR user_name = ? \n"
-				+ "OR m_authority.auth_status = ? \n"
-				+ "OR mail_address = ? )\n"
-				+ "AND delete_flg = 0 \n"
-				+ "ORDER BY user_id ASC";
+				+ "WHERE  delete_flg = 0 ";
 
-		// 権限名(authStatus)のパラメータの確認
-		String reAuthStatus = "";
-		switch (authStatus) {
-		case "admin":
-			reAuthStatus = "管理者";
-			break;
-		case "user":
-			reAuthStatus = "一般";
-			break;
-		case "all":
-			reAuthStatus = "*";
-			break;
+		// 入力された検索内容を格納する
+		List<Object> paramList = new ArrayList<Object>();
+
+		// ユーザIDが存在する場合
+		if (!userId.isEmpty()) {
+			sql += "AND user_id LIKE ? \n";
+			paramList.add("%" + userId + "%");
+		}
+		// ユーザ名が存在する場合
+		if (!userName.isEmpty()) {
+			sql += "AND user_name LIKE ? \n";
+			paramList.add("%" + userName + "%");
+		}
+		// 権限が選択されている場合
+		if (authStatus.equals("admin")) {
+			sql += "AND auth_status = ?";
+			authStatus = "管理者";
+			paramList.add(authStatus);
+		} else if (authStatus.equals("user")) {
+			sql += "AND auth_status = ?";
+			authStatus = "一般";
+			paramList.add(authStatus);
 		}
 
-		// ？の箇所を置換するデータの配列を定義
-		Object[] param = { userId, userName, reAuthStatus, mailAddress };
+		// メールアドレスが存在する場合
+		if (!mailAddress.isEmpty()) {
+			sql += "AND mail_address LIKE ?";
+			paramList.add("%" + mailAddress + "%");
+		}
+		// ユーザIDで昇順並び替え
+		sql += "ORDER BY user_id ASC;";
 
+		// ？の箇所を置換するデータの配列を定義
+		Object[] param = paramList.toArray();
 		// クエリを実行
 		List<Map<String, Object>> allUserSearchList = jdbcTemplate.queryForList(sql, param);
 
